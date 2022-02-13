a2cfad5ea8f05e08110bef18d1b53b2ed05c2c44
1000007201 <nishant.sharma5504@gmail.com>
2022-02-14 00:07:50 +0530

    [Nishant] Add:Stack operations are done

---
+++ DataStructureStack/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructureQueue;
+
+Stack stack = new Stack();
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Enter Value:\n1.Add Element in Stack\n2.Display Stack Elements\n3.Preform Peek Operation\n4.Perform Pop operation\n" +
+        "5.Perform IsEmpty Operation\n6.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("Enter how many Elements you want to add:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"Enter Element {i + 1}:");
+                int element = Convert.ToInt32(Console.ReadLine());
+                stack.Push(element);
+            }
+            break;
+        case 2:
+            stack.Display();
+            break;
+        case 3:
+            stack.Peek();
+            break;
+        case 4:
+            stack.Pop();
+            break;
+        case 5:
+            stack.IsEmpty();
+            break;
+        case 6:
+            flag = false;
+            break;
+    }
+}
+
---
+++ DataStructureStack/Stack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureQueue
+{
+    public class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if(this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine($"{value} Pushed to Stack");
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.Write($"{temp.data} ");
+                temp = temp.next;
+            }
+        }
+        public void Peek()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"{this.top.data} is on top of Stack");
+            }
+        }
+        public void Pop()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("Nothing to delete Stack is alreadt Empty");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"{this.top.data} is Poped Value");
+                this.top = this.top.next;
+            }
+        }
+        public void IsEmpty()
+        {
+            if(this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+    }
+}
