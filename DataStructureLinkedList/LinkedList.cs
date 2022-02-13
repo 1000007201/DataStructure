d9eb83aca5e0c2e94f7fd6d0a60b050d6682ae2c
1000007201 <nishant.sharma5504@gmail.com>
2022-02-13 21:49:01 +0530

    [Nishant] Add:Linked List Operations Done

---
+++ DataStructureLinkedList/LinkedList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    public class LinkedList
+    {
+        internal Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this .head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public void AddInReverse(int data)
+        {
+            Node newnode = new Node(data);
+            if(this.head == null)
+            {
+                this.head = newnode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newnode;
+                head.next = temp;
+            }
+        }
+        internal void Size()
+        {
+            Node temp = this.head;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("Lenght is " + count);
+        }
+        public int Search(int data)
+        {
+            Node node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+                if (node.data == data)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return -1;
+        }
+        internal void DeleteNodeParticularPosition(int position)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            Node temp = this.head;
+            if (position == 0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for (int i = 0; temp != null && i < position - 1; i++)
+            {
+                temp = temp.next;
+            }
+            if (temp == null || temp.next == null)
+            {
+                return;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+            Display();
+        }
+        public Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        public Node RemoveLastNode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newnode = head;
+            while (newnode.next.next != null)
+            {
+                newnode = newnode.next;
+            }
+            newnode.next = null;
+            return head;
+        }
+    }
+   
+}
---
+++ DataStructureLinkedList/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructureLinkedList;
+
+bool flag = true;
+LinkedList list = new LinkedList();
+while (flag)
+{
+    Console.WriteLine("Enter Value:\n1.Add Element.\n2.Display Your List." +
+        "\n3.Add in Reverse order.\n\n4.Get size of list.\n5.Search Elemnt." +
+        "\n6.Delete Element.\n7.Remove first node\n8.Remove Last node.\n9.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            Console.WriteLine("Enter Number of elements to be added:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"Enter Element {i + 1}:");
+                int element = Convert.ToInt32(Console.ReadLine());
+                list.Add(element);
+            }
+            break;
+        case 2:
+            list.Display();
+            break;
+        case 3:
+            Console.WriteLine("Enter Number of elements to be added:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number1; i++)
+            {
+                Console.WriteLine($"Enter Element {i + 1}:");
+                int revelement = Convert.ToInt32(Console.ReadLine());
+                list.AddInReverse(revelement);
+            }
+            break;
+        case 4:
+            list.Size();
+            break;
+        case 5:
+            Console.WriteLine("Enter Element You want to search:");
+            int element1 = Convert.ToInt32(Console.ReadLine());
+            int searchoption = list.Search(element1);
+            if (searchoption == -1)
+            {
+                Console.WriteLine("Element is not Present");
+            }
+            else
+            {
+                Console.WriteLine($"Element is present at {searchoption}");
+            }
+            break;
+        case 6:
+            Console.WriteLine("Enter Element to be deleted:");
+            int element2 = Convert.ToInt32(Console.ReadLine());
+            int position = list.Search(element2);
+            list.DeleteNodeParticularPosition(position);
+            break;
+        case 7:
+            list.RemoveFirstNode();
+            break;
+        case 8:
+            list.RemoveLastNode();
+            break;
+        case 9:
+            flag = false;
+            break;
+    }
+}
