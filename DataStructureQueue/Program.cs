eea7f41aa13c3f1accb4c63978db8681c8232c70
1000007201 <nishant.sharma5504@gmail.com>
2022-02-14 00:10:23 +0530

    [Nishant] Add:Queue operations performed

---
+++ DataStructureQueue/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructureQueue;
+
+Queue queue = new Queue();
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Enter Value:\n1.Add Element in Queue\n2.Display Queue Elements\n3.Perform Dequeue operation\n" +
+        "4.Size of Queue\n5.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch(option)
+    {
+        case 1:
+            Console.WriteLine("Enter how many Elements you want to add:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"Enter Element {i+1}:");
+                int element = Convert.ToInt32(Console.ReadLine());
+                queue.Enqueue(element);
+            }            
+            break;
+        case 2:
+            queue.Display();
+            break;
+        case 3:
+            queue.Dequeue();
+            break;
+        case 4:
+            queue.Size();
+            break;
+        case 5:
+            flag = false;
+            break;
+    }
+}
---
+++ DataStructureQueue/Queue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureQueue
+{
+    public class Queue
+    {
+        Node head = null;
+        public void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} inserted in Queue");
+        }
+        public void Display()
+        {
+            Node temp = head;
+            while(temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+        public void Dequeue()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Queue is already Empty");
+                return;
+            }
+            Console.WriteLine($"{head.data} is Dequeued");
+            head = head.next;
+        }
+        public void Size()
+        {
+            Node temp = head;
+            int count = 0;
+            while(temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            Console.WriteLine("Size of Queue:"+count);
+        }
+    }
+}
